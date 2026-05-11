a359735939eb4ab34f52434a3448d754aa30b5a0
Rafael Cepeda <rafaelc@apple.com>
2026-05-11 22:25:27 +0100

    Expand CNIOFreeBSD with mmsghdr, CMSG helpers, and pthread wrappers

--- Sources/CNIOFreeBSD/include/CNIOFreeBSD.h
+++ Sources/CNIOFreeBSD/include/CNIOFreeBSD.h
@@ -11,18 +11,43 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
-#ifndef C_NIO_FREEBSD_H                                                                                                                                                                                            
-#define C_NIO_FREEBSD_H                                                                                                                                                                                            
-                                                                                                                                                                                                                     
-#if defined(__FreeBSD__)                                                                                                                                                                                           
-#include <arpa/inet.h>                                                                                                                                                                                             
-#include <netinet/in.h>                                                                                                                                                                                            
-#include <netinet/ip.h>                                                                                                                                                                                            
-#include <sys/socket.h>                                                                                                                                                                                            
+#ifndef C_NIO_FREEBSD_H
+#define C_NIO_FREEBSD_H
 
-const char *CNIOFreeBSD_inet_ntop(int af, const void *src, char *dst, socklen_t size);                                                                                                                             
+#if defined(__FreeBSD__)
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <pthread.h>
+#include <pthread_np.h>
+
+typedef struct {
+    struct msghdr msg_hdr;
+    unsigned int msg_len;
+} CNIOFreeBSD_mmsghdr;
+
+typedef struct {
+    struct in6_addr ipi6_addr;
+    unsigned int ipi6_ifindex;
+} CNIOFreeBSD_in6_pktinfo;
+
+int CNIOFreeBSD_sendmmsg(int sockfd, CNIOFreeBSD_mmsghdr *msgvec, unsigned int vlen, int flags);
+int CNIOFreeBSD_recvmmsg(int sockfd, CNIOFreeBSD_mmsghdr *msgvec, unsigned int vlen, int flags, struct timespec *timeout);
+
+int CNIOFreeBSD_pthread_setname_np(pthread_t thread, const char *name);
+int CNIOFreeBSD_pthread_getname_np(pthread_t thread, char *name, size_t len);
+
+struct cmsghdr *CNIOFreeBSD_CMSG_FIRSTHDR(const struct msghdr *);
+struct cmsghdr *CNIOFreeBSD_CMSG_NXTHDR(struct msghdr *, struct cmsghdr *);
+const void *CNIOFreeBSD_CMSG_DATA(const struct cmsghdr *);
+void *CNIOFreeBSD_CMSG_DATA_MUTABLE(struct cmsghdr *);
+size_t CNIOFreeBSD_CMSG_LEN(size_t);
+size_t CNIOFreeBSD_CMSG_SPACE(size_t);
+
+const char *CNIOFreeBSD_inet_ntop(int af, const void *src, char *dst, socklen_t size);
 int CNIOFreeBSD_inet_pton(int af, const char *src, void *dst);
 
-#endif                                                                                                                                                                                                             
-                                                                                                                                                                                                                     
+#endif
 #endif
--- Sources/CNIOFreeBSD/shim.c
+++ Sources/CNIOFreeBSD/shim.c
@@ -14,14 +14,64 @@
 
 void CNIOFreeBSD_i_do_nothing_just_working_around_a_darwin_toolchain_bug(void) {}
 
-#if defined(__FreeBSD__)                                                                                                                                                                                           
-#include <arpa/inet.h>                                                                                                                                                                                             
-                                                                                                                                                                                                                     
-const char *CNIOFreeBSD_inet_ntop(int af, const void *src, char *dst, socklen_t size) {                                                                                                                            
-    return inet_ntop(af, src, dst, size);                                                                                                                                                                          
-}                                                                                                                                                                                                                  
-                  
-int CNIOFreeBSD_inet_pton(int af, const char *src, void *dst) {                                                                                                                                                    
+#if defined(__FreeBSD__)
+
+#include <CNIOFreeBSD.h>
+#include <assert.h>
+
+int CNIOFreeBSD_sendmmsg(int sockfd, CNIOFreeBSD_mmsghdr *msgvec, unsigned int vlen, int flags) {
+    return sendmmsg(sockfd, (struct mmsghdr *)msgvec, vlen, flags);
+}
+
+int CNIOFreeBSD_recvmmsg(int sockfd, CNIOFreeBSD_mmsghdr *msgvec, unsigned int vlen, int flags, struct timespec *timeout) {
+    return recvmmsg(sockfd, (struct mmsghdr *)msgvec, vlen, flags, timeout);
+}
+
+int CNIOFreeBSD_pthread_setname_np(pthread_t thread, const char *name) {
+    pthread_set_name_np(thread, name);
+    return 0;
+}
+
+int CNIOFreeBSD_pthread_getname_np(pthread_t thread, char *name, size_t len) {
+    pthread_get_name_np(thread, name, len);
+    return 0;
+}
+
+struct cmsghdr *CNIOFreeBSD_CMSG_FIRSTHDR(const struct msghdr *mhdr) {
+    assert(mhdr != NULL);
+    return CMSG_FIRSTHDR(mhdr);
+}
+
+struct cmsghdr *CNIOFreeBSD_CMSG_NXTHDR(struct msghdr *mhdr, struct cmsghdr *cmsg) {
+    assert(mhdr != NULL);
+    assert(cmsg != NULL);
+    return CMSG_NXTHDR(mhdr, cmsg);
+}
+
+const void *CNIOFreeBSD_CMSG_DATA(const struct cmsghdr *cmsg) {
+    assert(cmsg != NULL);
+    return CMSG_DATA(cmsg);
+}
+
+void *CNIOFreeBSD_CMSG_DATA_MUTABLE(struct cmsghdr *cmsg) {
+    assert(cmsg != NULL);
+    return CMSG_DATA(cmsg);
+}
+
+size_t CNIOFreeBSD_CMSG_LEN(size_t payloadSizeBytes) {
+    return CMSG_LEN(payloadSizeBytes);
+}
+
+size_t CNIOFreeBSD_CMSG_SPACE(size_t payloadSizeBytes) {
+    return CMSG_SPACE(payloadSizeBytes);
+}
+
+const char *CNIOFreeBSD_inet_ntop(int af, const void *src, char *dst, socklen_t size) {
+    return inet_ntop(af, src, dst, size);
+}
+
+int CNIOFreeBSD_inet_pton(int af, const char *src, void *dst) {
     return inet_pton(af, src, dst);
-}                                                                                                                                                                                                                  
+}
+
 #endif
