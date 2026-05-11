8650607aafa15e582ed7664a42b76dcba9f58cad
Rafael Cepeda <rafaelc@apple.com>
2026-05-11 13:58:39 +0100

    Adds missing license headers.

--- Sources/CNIOFreeBSD/shim.c
+++ Sources/CNIOFreeBSD/shim.c
@@ -1,3 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2026 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+// Xcode's Archive builds with Xcode's Package support struggle with empty .c files
+// (https://bugs.swift.org/browse/SR-12939).
+
 void CNIOFreeBSD_i_do_nothing_just_working_around_a_darwin_toolchain_bug(void) {}
 
 #if defined(__FreeBSD__)                                                                                                                                                                                           
